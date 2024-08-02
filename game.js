c91952a1f9ce2e238e19554d49a3b46a1dd167d1
CanerYesiltas <caneryesiltas1@gmail.com>
2024-08-02 01:05:20 -0400

    last update

--- game.js
+++ game.js
@@ -1,52 +1,74 @@
-let rastgeleSayi = Math.ceil(Math.random() * 20); // 1 ile 20 arasinda random sayi ureten fonksiyon
-console.log(rastgeleSayi); // Kontrol için eklendi
+//* pc ye sayı tutturduk 1-20 arasında olabilmesi için 20 ile çarpıp yukarı (ceil) yuvarladık
+
+let rastgeleSayi = Math.ceil(Math.random() * 20);
+console.log(rastgeleSayi);
 
 let message = document.querySelector(".msg");
 
-let myScore = 10; let enYuksekSkor = 0;
+let myScore = 10;
+let enYuksekSkor = 0;
 
+//* her check butonuna basınca olacaklar
+
+//    document.querySelector(".check").onclick=()=>{}
 document.querySelector(".check").addEventListener("click", () => {
-  // click eventi gerceklestiginde bu fonksiyon baslayacak arrow fonksiyon isimsiz anonim parametresiz!
+  const tahmin = document.querySelector(".guess").value;
 
-  const tahmin = Number(document.querySelector(".guess").value); // fonksiyon icinde tanimlamak daha sagliklidir global scope ne kadar az tanimlarsak o kadar iyi sadece tanim bu !
-
+  //?tahmin girilmeden check e basılmasın
   if (!tahmin) {
-    message.textContent = "lutfen bir tahmin giriniz";
-  } else if (tahmin === rastgeleSayi) {
-    message.textContent = "tebrikler bildiniz🎉";
-    
-    document.querySelector("body").style.backgroundColor="green"
-    document.querySelector(".number").textContent=rastgeleSayi;
+    message.textContent = "lütfen bir tahmin giriniz";
+  } else if (tahmin == rastgeleSayi) {
+    //?tahmin doğruysa
+
+    message.textContent = "Tebrikler Bildiniz..🎉";
+    document.querySelector("body").style.backgroundColor = "green";
+
+    document.querySelector(".number").textContent = rastgeleSayi;
+
     document.querySelector(".check").disabled = true;
-    // buton tiklanmaz kapali olur disabled komutu ile !
-    
-    if( myScore>enYuksekSkor) {
-      enYuksekSkor=myScore;
-      document.querySelector('.top-score').textContent=enYuksekSkor;
+
+    //!!!!!!! enYüksekSkor u güncelle
+
+    if (myScore > enYuksekSkor) {
+      enYuksekSkor = myScore;
+
+      document.querySelector(".top-score").textContent = enYuksekSkor;
     }
   } else {
-    if(myScore>1) {
+    //?tahmin yanlışsa
+    //?myScore 1 den aşağı düşmemeli, 1 hakkım varken yine yanlış tahminde bulunursam else e düşmeliyim
+    if (myScore > 1) {
       myScore--;
-      
-      document.querySelector('.my-score').textContent=myScore; // tempral literate ilede kullanilabilirdi
-      tahmin > rastgeleSayi ? (message.textContent="azalt") : (message.textContent="arttir");
+
+      document.querySelector(".my-score").textContent = myScore;
+
+      tahmin > rastgeleSayi
+        ? (message.textContent = "Azalt")
+        : (message.textContent = "Arttir");
     } else {
-      document.querySelector("body").style.backgroundColor="red";
-      document.querySelector(".my-score").textContent=0;
-      message.textContent="oyunu kaybettiniz";
-      document.querySelector(".check").disabled=true;
+      document.querySelector("body").style.backgroundColor = "red";
+      document.querySelector(".my-score").textContent = 0;
+
+      message.textContent = "Oyunu kaybettiniz";
+      document.querySelector(".check").disabled = true;
     }
   }
 });
 
-document.querySelector(".again").onclick =() => {
-  rastgeleSayi= Math.ceil(Math.random()*20);
+//* Again butonuna basınca olacaklar
+document.querySelector(".again").onclick = () => {
+  rastgeleSayi = Math.ceil(Math.random() * 20);
   console.log(rastgeleSayi);
-  message.textContent="oyun yeni oyuncu icin tekrar basliyor";
-  document.querySelector('body').style.backgroundColor= "#2d3436";
-  document.querySelector(".number").textContent="?";
-  document.querySelector(".guess").value="";
-  myScore=10;
+  message.textContent = "Oyun yeni oyuncu için tekrar başlıyor";
+
+  document.querySelector("body").style.backgroundColor = "#2d3436";
+
+  document.querySelector(".number").textContent = "?";
+
+  document.querySelector(".guess").value = "";
+
+  myScore = 10;
+
   document.querySelector(".my-score").textContent = myScore;
   document.querySelector(".check").disabled = false;
-}
+};
